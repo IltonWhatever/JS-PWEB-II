f747ef64e18d4cb4f5c9aa8f3f3ccac23cb7d0cc
IltonWhatever <101358506+IltonWhatever@users.noreply.github.com>
2023-10-02 19:57:55 -0300

    Add files via upload

---
+++ Constante.js
@@ -0,0 +1,8 @@
+//  Exemplo de uso Constantes, variaveis const geralmente são declaradas em maiusculas 
+const ANIVESARIO = '18.04.1982';
+ANIVESARIO = '21.01.2001'; // erro, não é possivel alterar o valor de uma constante
+
+// 
+const COLOR_RED = '#F00';
+let color = COLOR_RED;
+alert(color); // #F00
