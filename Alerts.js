1525c8b9b997178d15db3dc243196e28decd4143
IltonWhatever <101358506+IltonWhatever@users.noreply.github.com>
2023-10-03 21:49:59 -0300

    Add files via upload

---
+++ Alerts.js
@@ -0,0 +1,11 @@
+// Caixa de Alerta com um imput de infornações
+let idade = prompt("Digite sua Idade");
+alert(`Sua idade é ${idade}`);
+
+// Alerta com o objetivo de anteceder a conclusão de uma tarefa, por exemplo fechar uma aba
+let acabar = confirm("Tem certeza que vai fazer isso ?")
+alert( acabar )
+
+// Especie de criar IF e ELSE mais rapido
+let age = 17
+alert( age >=18 ? "Permitido" : "Proibido")
