a3fda4c2c2808bf353951ed278f35d6ebabd3163
IltonWhatever <101358506+IltonWhatever@users.noreply.github.com>
2023-10-09 19:53:12 -0300

    Add files via upload

---
+++ Q9/script.js
@@ -0,0 +1,9 @@
+const numero = prompt("Digite um numero: ");
+
+if(numero >0){
+    alert(`O numero ${numero} é positivo`)
+}else if(numero <0){
+    alert(`O numero ${numero} é negativo`)
+}else{
+    alert(`O numero ${numero} é 0`)
+}
