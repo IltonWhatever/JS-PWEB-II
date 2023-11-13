6440f230aa0e340612a3309ad91e60334afd59eb
IltonWhatever <101358506+IltonWhatever@users.noreply.github.com>
2023-11-13 19:42:03 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+const buscarDoguinho = (e) =>{
+    e.preventDefault()
+    const xhr = new XMLHttpRequest()
+    xhr.open('GET', 'https://api.thedogapi.com/v1/images/search?limit=10')
+    xhr.onreadystatechange = () => {
+        if(xhr.readyState === 4){
+            if(xhr.status === 200){
+                const dogs = JSON.parse(xhr.responseText)
+                dogs.forEach(dog => {
+                    const img = document.createElement('img')
+                    img.src = dog.url
+                    document.querySelector('#Doguinhos').appendChild(img)
+                })
+            } else{
+                alert('Erro na requisição!')
+            }
+        }
+    }
+    xhr.send()
+}
+const btnMostrar = document.querySelector('#mostrar-dogs')
+btnMostrar.addEventListener('click', buscarDoguinho)
