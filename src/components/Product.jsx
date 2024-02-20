73d76c033b46ddea4bacabe1633e75f65b3e53a8
IltonWhatever <101358506+IltonWhatever@users.noreply.github.com>
2024-02-20 20:50:27 -0300

    Add files via upload

---
+++ src/components/Product.jsx
@@ -0,0 +1,20 @@
+function Product( { p_info } ){
+    return (
+        <div className="product">
+            <div className="left">
+                <img id="draft" src={p_info.thumbail} alt="" />
+                <ul id="images">
+                    { p_info.images.map(
+                        (img, idx) => <li key={idx}> <img src={img} alt=""/> </li>
+                    )}
+                </ul>
+            </div>
+            <div className="right">
+                <h1>{p_info.title}</h1>
+                <h2>R$ {p_info.price}</h2>
+                <p>{p_info.description}</p>
+                <a href="">Compre Agora</a>
+            </div>
+        </div>
+    )
+};
