9e633cae7bc40388b0ca6fb869b94d0d2b1bd340
IltonWhatever <101358506+IltonWhatever@users.noreply.github.com>
2024-02-20 21:15:33 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import Product from './components/Product'
 
 function App() {
-  <>
-     Produtos
-  </>
+  const [product, setProduct] = useState(null)
+  
+  useEffect(() => {
+    console.log("Searching for Data")
+    fetch("https://dummyjson.com/products/1").then(resp =>{
+      console.log("Data is ready")
+      if(resp.ok){
+        return resp.json()
+      }
+      throw new Error("Error")
+    })
+    .then(dados => setProduct(dados))
+    .catch(e => console.error(e))
+
+  },[])
+  
+  return(
+    <>
+      { product ? (
+        <Product p_info={product}/>
+      ):(
+        "Loading..."
+      ) }
+    </>
+  )
   
 }
 
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -2,7 +2,7 @@ function Product( { p_info } ){
     return (
         <div className="product">
             <div className="left">
-                <img id="draft" src={p_info.thumbail} alt="" />
+                <img id="draft" src={p_info.thumbnail} alt="" />
                 <ul id="images">
                     { p_info.images.map(
                         (img, idx) => <li key={idx}> <img src={img} alt=""/> </li>
@@ -13,8 +13,10 @@ function Product( { p_info } ){
                 <h1>{p_info.title}</h1>
                 <h2>R$ {p_info.price}</h2>
                 <p>{p_info.description}</p>
-                <a href="">Compre Agora</a>
+                <a href="#">Compre Agora</a>
             </div>
         </div>
     )
 };
+
+export default Product;
